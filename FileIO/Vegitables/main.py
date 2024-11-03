c350b468ca93751d60250d30b21b85a741337c32
Prashant-kumar-321 <pk6176838@gmail.com>
2024-11-04 01:30:59 +0530

    Practice csv file operation by implementing crud opearation

--- FileIO/Vegitables/main.py
+++ FileIO/Vegitables/main.py
@@ -6,12 +6,11 @@ def main():
     # name = input("What is the name of the vegitable ? ")
     # price = input("What's the price of the vegitable ? ")
 
-    # vegitable = {"name": name, "price": price}
-
+    # vegitable = {"name": 'LadyFinger', "price": '60'}
     # vegitables.add_vegitables(FILE_PATH, vegitable)
-    # vegitables.update_vegitable(FILE_PATH, {"name": "Onion", "price": '80'}, {"name": "Onion", "price": '100'})
+    # vegitables.update_vegitable(FILE_PATH, {"name": "Cabage", "price": '50'}, {"name": "Cabage", "price": '100'})
 
-    vegitables.delete_vegitable(FILE_PATH, {"name": "Onion", "price": "100"})
+    vegitables.delete_vegitable(FILE_PATH, {"name": "Tomato", "price": "100"})
     vegitables.print_vegitables(FILE_PATH)
 
 
--- FileIO/Vegitables/vegitables.py
+++ FileIO/Vegitables/vegitables.py
@@ -26,47 +26,40 @@ def print_vegitables(file_path):
         
 
 def update_vegitable(file_path, old_vegitable, new_vegitable): 
-    
-    org_file = open(file_path)
-    temp_file = open("./temporary.csv", 'w') # create a temporary file
+    temporary_file = './temporary.csv'
 
-    temp_file.write('name,price\n') # Create a header of the csv 
+    with open(file_path, 'r') as org_file, open(temporary_file, "w") as temp_file: 
+        reader = csv.DictReader(org_file)
+        writer = csv.DictWriter(temp_file, fieldnames=reader.fieldnames)
+        writer.writeheader()
 
-    reader = csv.DictReader(org_file)
-    writer = csv.DictWriter(temp_file, fieldnames=['name', 'price'])
-
-    for row in reader: 
-        if row == old_vegitable:
-            writer.writerow(new_vegitable)
-        else: 
+        for row in reader: 
+            if row == old_vegitable:
+                writer.writerow(new_vegitable)
+                continue
+            
             writer.writerow(row)
     
     # delete the original file 
     os.remove(file_path)
 
     # rename the temporary file to the original file 
-    os.rename('./temporary.csv', file_path)
-    temp_file.close()
+    os.rename(temporary_file, file_path)
 
 
 def delete_vegitable(file_path, vegitable): 
     temporary_file = './temporary.csv'
+    with open(file_path) as org_file, open(temporary_file, 'w') as temp_file: 
+        reader = csv.DictReader(org_file)
+        writer = csv.DictWriter(temp_file, fieldnames=reader.fieldnames)
+        writer.writeheader()
 
-    org_file = open(file_path)
-    temp_file = open(temporary_file, 'w')
-
-    temp_file.write('name,price\n') # csv header
-
-    reader = csv.DictReader(org_file)
-    writer = csv.DictWriter(temp_file, fieldnames=['name', 'price'])
-
-    for row in reader: 
-        if row != vegitable: 
-            writer.writerow(row)
+        for row in reader: 
+            if row != vegitable: 
+                writer.writerow(row)
     
     # remove the original file 
     os.remove(file_path)
 
     # rename temporary file 
     os.rename(temporary_file, file_path)
-    temp_file.close()
